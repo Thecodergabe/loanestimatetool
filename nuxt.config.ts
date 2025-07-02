201d3ffed6bd9c5f6073e24c8e146b04e5e399c5
brogrammer <thecodergabe@gmail.com>
2025-07-01 19:55:29 -0700

    Add config for cloudflare pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-
+  nitro: {
+    preset: 'cloudflare-pages',
+    serveStatic: true
+  },
+  ssr: true,
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
